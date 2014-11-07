749e44f2f231a8b70f2c888b68b3ce6cd4bd400a
kaltirim <kaltirim@lab.grid.auth.gr>
2014-11-07 12:46:06 +0200

    minor changes

--- Java Applications/mongoDbAdapter/src/main/java/db/mongodbadapter/App.java
+++ Java Applications/mongoDbAdapter/src/main/java/db/mongodbadapter/App.java
@@ -4,9 +4,9 @@ public class App {
 
     public static void main(String[] args) {
 
-        if (args.length < 2) 
+        if (args.length < 1) 
         {
-            System.out.println("Proper use: <new_files_directory> <backup_files_directory>");
+            System.out.println("Proper use: <category_directory>");
         }
         else 
         {
@@ -14,7 +14,7 @@ public class App {
             //The directory containing the new json files that need to be inserted in the db
             final String new_folder_path = "/usr/lib/twitter-project/" + args[0] + "/new/";
             //the directory the new json will be moved into after they are inserted
-            final String backup_folder_path = "/usr/lib/twitter-project/" + args[1] + "/new/";
+            final String backup_folder_path = "/usr/lib/twitter-project/" + args[0] + "/backup/";
         }
 
     }
